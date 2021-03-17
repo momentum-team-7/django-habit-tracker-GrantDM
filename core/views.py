1710606c27a0329895dbd738707c1d7c5addd55c
GrantDM <mullen.d.grant@gmail.com>
2021-03-17 11:34:57 -0400

    first commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import User, Habit
+
+
 
 # Create your views here.
+
+def homepage(request):
+    habits = Habit.objects.all()
+    return render(Request, 'homepage.html', {'habits': habits})
